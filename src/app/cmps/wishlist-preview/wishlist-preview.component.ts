064ba49e9daca5fd74fe9ed7f75ba808243b8f94
Yohai Casoy Korem <118823808+YohaiKorem@users.noreply.github.com>
2023-08-22 16:59:09 +0300

    refactored modal service & added wishlist cmps

--- src/app/cmps/wishlist-preview/wishlist-preview.component.ts
+++ src/app/cmps/wishlist-preview/wishlist-preview.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StayService } from 'src/app/services/stay.service';
 
 @Component({
   selector: 'wishlist-preview',
   templateUrl: './wishlist-preview.component.html',
-  styleUrls: ['./wishlist-preview.component.scss']
+  styleUrls: ['./wishlist-preview.component.scss'],
 })
-export class WishlistPreviewComponent {
+export class WishlistPreviewComponent implements OnInit {
+  constructor(private stayService: StayService) {}
+  @Input() wishlist!: any;
+  stayImgsForDisplay!: string[];
 
+  ngOnInit(): void {
+    this.loadStayImages();
+  }
+
+  loadStayImages(): void {
+    this.stayImgsForDisplay = [];
+
+    this.wishlist.forEach((stayId: string) => {
+      this.stayService.getStayById(stayId).subscribe((stay) => {
+        this.stayImgsForDisplay.push(stay.imgUrls[0]);
+      });
+    });
+  }
 }
