2c797384748837f2dcecb6df3fff9705307452a4
Yohai Casoy Korem <118823808+YohaiKorem@users.noreply.github.com>
2023-09-26 18:47:07 +0300

    started implementing orderList

--- src/app/cmps/order-list/order-list.component.ts
+++ src/app/cmps/order-list/order-list.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Order } from 'src/app/models/order.model';
 
 @Component({
   selector: 'order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.scss'],
 })
-export class OrderListComponent {}
+export class OrderListComponent {
+  @Input() orders: Order[] = [];
+}
--- src/app/cmps/order-preview/order-preview.component.ts
+++ src/app/cmps/order-preview/order-preview.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Order } from 'src/app/models/order.model';
 
 @Component({
   selector: 'order-preview',
   templateUrl: './order-preview.component.html',
-  styleUrls: ['./order-preview.component.scss']
+  styleUrls: ['./order-preview.component.scss'],
 })
 export class OrderPreviewComponent {
-
+  @Input() order!: Order;
 }
