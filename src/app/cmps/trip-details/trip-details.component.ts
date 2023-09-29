8787d1496d088a3c06a87bf56d8af7bb4ef5b093
Yohai Casoy Korem <118823808+YohaiKorem@users.noreply.github.com>
2023-09-29 14:15:49 +0300

    started styling trip details

--- src/app/cmps/trip-details/trip-details.component.ts
+++ src/app/cmps/trip-details/trip-details.component.ts
@@ -6,7 +6,9 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { Order } from 'src/app/models/order.model';
-
+import { StayService } from 'src/app/services/stay.service';
+import { take } from 'rxjs';
+import { Stay } from 'src/app/models/stay.model';
 @Component({
   selector: 'trip-details',
   templateUrl: './trip-details.component.html',
@@ -15,14 +17,25 @@ import { Order } from 'src/app/models/order.model';
 export class TripDetailsComponent implements OnInit {
   @Input() trip: Order | null = null;
   formattedDates!: { start: string; end: string };
-
+  stay!: Stay;
+  constructor(private stayService: StayService) {}
   ngOnInit() {
+    this.getStay();
     this.formattedDates = this.getFormattedDateForDisplay();
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['trip'])
+    if (changes['trip']) {
       this.formattedDates = this.getFormattedDateForDisplay();
+      this.getStay();
+    }
+  }
+
+  getStay() {
+    this.stayService
+      .getStayById(this.trip?.stay._id!)
+      .pipe(take(1))
+      .subscribe((stay: Stay) => (this.stay = stay));
   }
 
   getFormattedDateForDisplay() {
