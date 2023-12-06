4ffe5642140fa918a31eada2db67aaf1f2f44f65
Yohai Casoy Korem <118823808+YohaiKorem@users.noreply.github.com>
2023-12-06 12:37:13 +0200

    changed builder and replaced comment type in scss

--- src/app/services/geocoding.service.ts
+++ src/app/services/geocoding.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map, of, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
@@ -8,7 +9,7 @@ export class GeocodingService {
   constructor(private http: HttpClient) {}
 
   public getLatLng(place: string): Observable<any> {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${place}&key=AIzaSyD6VYrEZ0nY107CEP22L0mZ0gmu-7eLf1Y`;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${place}&key=${environment.geocodeAPI}-7eLf1Y`;
     return this.http.get(url).pipe(
       map((response: any) => {
         if (response.status === 'OK') {
--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,17 @@ module.exports = {
         test: /\.scss$/,
         loader: "postcss-loader",
         options: {
-          ident: "postcss",
-          syntax: "postcss-scss",
-          plugins: () => [
-            require("postcss-import"),
-            require("autoprefixer"),
-            purgecss({
-              content: ["./**/*.html"],
-              // Example to let PurgeCss know how to exclude cdk and mat prefixes if your using Angular CDK and Angular Material
-              whitelistPatterns: [/^cdk-|mat-/],
-            }),
-          ],
+          postcssOptions: {
+            syntax: "postcss-scss",
+            plugins: [
+              require("postcss-import"),
+              require("autoprefixer"),
+              purgecss({
+                content: ["./**/*.html"],
+                whitelistPatterns: [/^cdk-|mat-/],
+              }),
+            ],
+          },
         },
       },
     ],
